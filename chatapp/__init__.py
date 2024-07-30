9d71e819de4f9bdf91a7f30fff081c23fcb1a8c9
leoworqu <132668626+leoworqu@users.noreply.github.com>
2024-07-30 08:28:23 +0300

    Update __init__.py

--- chatapp/__init__.py
+++ chatapp/__init__.py
@@ -9,10 +9,10 @@ app.config['SECRET_KEY'] = '46af1a55a3a3992bdb12d4658e9a96e7'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///webapp.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+socketIO = SocketIO(app)
 login_Manager = LoginManager(app)
 login_Manager.login_view = 'login'
 login_Manager.login_message_category = 'error'
-socketIO = SocketIO(app)
 
 
 from chatapp import routes
